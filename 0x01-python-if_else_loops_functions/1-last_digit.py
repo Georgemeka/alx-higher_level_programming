e81224e45bc084d8e59a73a3d27c3a77d08c9f1e
George Iheanyi <georgeiheanyi@gmail.com>
2023-03-09 03:31:34 -0800

    Last digit python

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = abs(number) % 10
-if last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+no = number
+if no < 0:
+    no *= -1
+last_digit = no % 10
+if last_digit > 5 and number < 0:
+    last_digit *= -1
+    print(f"Last digit of {number:d} is {last_digit:d} "
+          f"and is less than 6 and not 0")
+elif last_digit > 5 and number > 0:
+    print(f"Last digit of {number:d} is {last_digit:d} and is greater than 5")
 elif last_digit == 0:
-    print(f"Last digit of {number} is {last_digit} and is 0")
-else:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+    print(f"Last digit of {number:d} is {last_digit:d} and is 0")
+elif last_digit < 6 and last_digit != 0 and number < 0:
+    last_digit *= -1
+    print(f"Last digit of {number:d} is {last_digit:d} "
+          f"and is less than 6 and not 0")
+elif last_digit < 6 and last_digit != 0 and number > 0:
+    print(f"Last digit of {number:d} is {last_digit:d} "
+          f"and is less than 6 and not 0")
