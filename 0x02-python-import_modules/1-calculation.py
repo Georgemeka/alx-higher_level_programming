9fe277f2e84e7acc95d692645d30c9d5e8e1164b
George Iheanyi <georgeiheanyi@gmail.com>
2023-03-09 17:36:03 -0800

    Calculator

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -8,10 +8,39 @@ def main():
     b = 5
 
     print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
 
 
 if __name__ = "__main__":
     main()
+
+
+def submain():
+    a = 10
+    b = 5
+
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+
+
+if __name__ = "__main__":
+    submain()
+
+
+def mulmain():
+    a = 10
+    b = 5
+
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+
+
+if __name__ = "__main__":
+    mulmain()
+
+
+def dmain():
+    a = 10
+    b = 5
+    print("{} / {} = {}".format(a, b, div(a, b)))
+
+
+if __name__ = "__main__":
+    dmain()
