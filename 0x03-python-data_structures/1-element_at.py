c2ca1d4025b55877d87699d87c19d41cb4136ac8
George Iheanyi <georgeiheanyi@gmail.com>
2023-03-13 23:28:07 -0700

    Retrieve element from a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    for i in range(len(my_list)):
-        idx = my_list[i]
-        if idx < 0 or idx > len(my_list)-1:
+        if idx < 0 or idx >= len(my_list):
             return None
         else:
             return my_list[idx]
