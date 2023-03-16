472ba1ea99c69d31e34b2c2d70b9c12c6826cba4
George Iheanyi <georgeiheanyi@gmail.com>
2023-03-16 06:03:00 -0700

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
+    if a_dictionary:
+        return (max(a_dictionary, key=a_dictionary.get))
     else:
-        max_key = None
-        max_value = float('-inf')
-        for key, value in a_dictionary.items():
-            if value > max_value:
-                max_key = key
-                max_value = value
-            return max_key
+        return None
